fcefec2350884ee7fac76bcac668e6bda023e9db
winZD <puntica007@gmail.com>
2024-07-27 16:50:04 +0200

    Fixed layout overflow

--- oliva/app/page.tsx
+++ oliva/app/page.tsx
@@ -5,12 +5,10 @@ import LandingImg from "../public/charts.svg";
 
 export default function Home() {
   return (
-    <main className="h-screen">
-      <header className="max-w-full px-4 sm:px-8 py-6 bg-primary">
-        Header
-      </header>
+    <main className="h-screen flex flex-col">
+      <header className="w-full px-4 sm:px-8 py-6 bg-primary">Header</header>
 
-      <section className="max-w-full py-6 mx-auto px-4 justify-center sm:justify-start sm:px-8 grid lg:grid-cols-[1fr,400px] items-center">
+      <section className="w-full py-6 mx-auto h-full px-4 justify-center sm:justify-start sm:px-8 grid lg:grid-cols-[1fr,400px] items-center">
         <div>
           <h1 className="capitalize text-4xl md:text-7xl font-bold">
             olive <span className="text-primary">tracking</span> app
