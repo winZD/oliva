860899c09d2128268b2b48d9139f54b5f9a39e62
winZD <puntica007@gmail.com>
2024-08-08 15:20:39 +0200

    added harvest model

---
+++ oliva/utils/harvestActions/actions.ts
@@ -0,0 +1,17 @@
+"use server";
+
+import { redirect } from "next/navigation";
+import prisma from "../db";
+import { HarvestType } from "../models/harvestModel";
+export const getHarvestsAction = async (): Promise<HarvestType[] | null> => {
+  try {
+    const harvests: HarvestType[] = await prisma.harvest.findMany({
+      orderBy: { year: "desc" },
+    });
+
+    return harvests;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
---
+++ oliva/utils/models/harvestModel.ts
@@ -0,0 +1,7 @@
+export type HarvestType = {
+  id: string;
+  year: Date;
+  quantity: number;
+  position: string;
+  oil_percentage: number;
+};
