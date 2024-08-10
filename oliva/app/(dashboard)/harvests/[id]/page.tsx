fbe67246cb31d91f5e615b372a96a1184646234d
winZD <puntica007@gmail.com>
2024-08-10 14:34:06 +0200

    Initial setup for create harvest

---
+++ oliva/app/(dashboard)/harvests/[id]/page.tsx
@@ -0,0 +1,9 @@
+import CreateHarvestForm from "@/components/CreateHarvestForm";
+import { CustomFormField } from "@/components/FormComponents";
+import React from "react";
+
+const Harvest = () => {
+  return <CreateHarvestForm />;
+};
+
+export default Harvest;
--- oliva/components/CreateHarvestForm.tsx
+++ oliva/components/CreateHarvestForm.tsx
@@ -1,7 +1,43 @@
-import React from "react";
+"use client";
+import { useForm } from "react-hook-form";
+import { CustomFormField } from "./FormComponents";
+import { createAndEditOrchardFormSchema } from "@/utils/actions/orchardActions/validations";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { CreateAndEditOrchardType } from "@/utils/models/orchardModel";
+import { z } from "zod";
+import { Form } from "./ui/form";
 
 const CreateHarvestForm = () => {
-  return <div></div>;
+  const formSchema = z.object({
+    name: z.string().min(2, {
+      message: "Name must be at least 2 characters.",
+    }),
+  });
+  const form = useForm<any>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+  function onSubmit(values: any) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+
+    console.log(values);
+  }
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="bg-muted p-8 rounded"
+      >
+        <h2 className="capitalize font-semibold text-4xl mb-6">add harvest</h2>
+        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3 items-start">
+          <CustomFormField name={"name"} control={form.control} />
+        </div>
+      </form>
+    </Form>
+  );
 };
 
 export default CreateHarvestForm;
