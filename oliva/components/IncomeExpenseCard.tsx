f39d43cd48dc42eefce005416a4175d542428fb9
winZD <puntica007@gmail.com>
2024-08-21 14:33:08 +0200

    removed code

--- oliva/components/IncomeExpenseCard.tsx
+++ oliva/components/IncomeExpenseCard.tsx
@@ -17,14 +17,6 @@ import {
 import { IncomeAndExpense } from "@/utils/models/incomeAndExpenseModel";
 
 const IncomeExpenseCard = ({ data }: { data: IncomeAndExpense[] }) => {
-  const chartData = [
-    { month: "January", desktop: 186, mobile: 80 },
-    { month: "February", desktop: 305, mobile: 200 },
-    { month: "March", desktop: 237, mobile: 120 },
-    { month: "April", desktop: 73, mobile: 190 },
-    { month: "May", desktop: 209, mobile: 130 },
-    { month: "June", desktop: 214, mobile: 140 },
-  ];
   const chartConfig = {
     expense: {
       label: "Expense",
