2a6e984801600ec953012169a4684e71c371e557
winZD <puntica007@gmail.com>
2024-08-24 15:27:48 +0200

    more setup

--- oliva/app/(dashboard)/incomes-and-expenses/[id]/page.tsx
+++ oliva/app/(dashboard)/incomes-and-expenses/[id]/page.tsx
@@ -1,5 +1,21 @@
-const HarvestAndIncome = () => {
-  return <div></div>;
+import { getIncomeAndExpenseAction } from "@/utils/actions/incomeAndExpenseActions/actions";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+
+const IncomeAndExpenseDetailsPage = async ({
+  params,
+}: {
+  params: { id: string };
+}) => {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["incomeAndExpense", params?.id],
+    queryFn: () => getIncomeAndExpenseAction(params?.id),
+  });
+  return <HydrationBoundary state={dehydrate(queryClient)}></HydrationBoundary>;
 };
 
-export default HarvestAndIncome;
+export default IncomeAndExpenseDetailsPage;
